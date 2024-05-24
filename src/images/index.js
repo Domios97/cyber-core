d817c09d70dc2109c26bf3b8344a3454c255d7fc
Mac <mac@MacBook-Pro-de-Mac.local>
2024-05-24 15:27:52 +0100

    commiting the site's navbar

---
+++ src/images/index.js
@@ -0,0 +1,20 @@
+import loupe from './loupe.png';
+import barres from './barres.png';
+import panier from './panier.png';
+import favori from './favori.png'; 
+import account from './account.png';
+import sitelogo from './sitelogo.png';
+import shop from './shop.png';
+import appstore from './appstore.png';
+import googleplay from './googleplay.png';
+export {
+  loupe,
+  barres,
+  panier,
+  favori,
+  account,
+  sitelogo,
+  shop,
+  appstore,
+  googleplay,
+};
