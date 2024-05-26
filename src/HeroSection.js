7dc772371e7c0e88659acebae545525c68148ae7
Chihab Mahfouf <ghostkt56@gmail.com>
2024-05-26 15:28:54 +0100

    commit in the product cart

---
+++ src/HeroSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HeroSection.css'; // Import CSS file for styling (create this file)
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1>Welcome to Our Website</h1>
+        <p></p>
+   
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
