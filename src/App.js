a3758f6bdcc8f6b1ec09845874e73ec5d4c85d40
Chihab Mahfouf <ghostkt56@gmail.com>
2024-05-30 10:16:35 +0100

    New CheckoutPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CategoryFilter from './Components/CategoryFilter/CategoryFilter';
-import FilterSort from './Components/FilterSort/FilterSort';
-import SearchBar from './Components/SearchBar/SearchBar';
 import Navbar from './Components/NavBar/Navigbar';
 import SiteFooter from './Components/Footer/sitefooter';
-import HomePage from '../src/Pages/HomePage/HomePage';
 import CheckoutPage from './Pages/CheckoutPage/CheckoutPage';
 
 
