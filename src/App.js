9a29356d7980f0aab006e33c8af1fdd0b8fe4ed9
Mac <mac@MacBook-Pro-de-Mac.local>
2024-06-01 02:04:25 +0100

    changing the font for the product details page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import Navbar from './Components/NavBar/Navigbar';
 import SiteFooter from './Components/Footer/sitefooter';
 import HomePage from './Pages/HomePage/HomePage';
+import ProductDetailsPage from './Pages/productDetails/productDetails'
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <HomePage />
-      <SiteFooter />
+      <ProductDetailsPage />
     </div>
 
   );
