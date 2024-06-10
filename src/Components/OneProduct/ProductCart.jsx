9e925edf54f326738cb6da846440a58fa4c12947
Chihab Mahfouf <ghostkt56@gmail.com>
2024-06-10 10:43:49 +0100

    ShopPage

--- src/Components/OneProduct/ProductCart.jsx
+++ src/Components/OneProduct/ProductCart.jsx
@@ -1,52 +1,22 @@
 import React from 'react';
+import AddCartButt from '../AddToCartButton/AddCartButt';
 import './ProductCart.css';
+import { router, headset } from '../../images';
 import AddWishlistButt from '../AddWishlistButton/AddWishlistButt';
-import headsetImage from '../../images/headset.jpg';
-import SeeDetail from '../SeeDetailsButton/SeeDetail';
 
-const products = [
-  {
-    id: 1,
-    image: headsetImage,
-    title: 'Pc',
-    price: 400,
-    category: 'Electronics'
-  },
-  {
-    id: 1,
-    image: headsetImage,
-    title: 'Router',
-    price: 300,
-    category: 'Phones'
-  },  {
-    id: 1,
-    image: headsetImage,
-    title: 'Test',
-    price: 200,
-    category: 'Tablets'
-  },
-];
-const ProductCart = ({ products }) => {
+function ProductCart() {
   return (
     <div className="product-cart-container">
-      {products.map((product) => (
-        <div className="product-cart" key={product.id}>
-          <img src={product.image} alt={product.title} />
-          <h3>{product.title}</h3>
-          <p> {product.category}</p>
-          <p className="price">{product.price} DZD</p>
-          <div className="actions">
-
-          <button className="see-details">See Details</button>
-          </div>
-
-          <AddWishlistButt customAddWishlist="product-detail" />
-        
-        </div>
-      ))}
+      <div className="product-cart">
+        <AddWishlistButt customAddWishlist="product-detail" />
+        <img src={headset} alt="Headset" />
+        <p>Router</p>
+        <p id='price'>234 700 DZD</p>
+        <AddCartButt addCartHomePage="latest-products" />
+      </div>
     </div>
   );
-};
+}
 
 export { products };
 export default ProductCart;
